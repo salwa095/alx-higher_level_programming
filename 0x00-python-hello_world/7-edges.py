b15b7b3f5e834276968b28be51beb68dddc42067
salwa <salwa095>
2024-02-12 12:42:21 +0000

    edges

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(word_first_3[:3])
+print(word_last_2[2:])
+print(middle_word[1:-1])
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
